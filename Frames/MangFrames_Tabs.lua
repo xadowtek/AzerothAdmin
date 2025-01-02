f6e699ae55b61e5ff66e9284050a91ea2bc1c052
xadow <72521329+xadowtek@users.noreply.github.com>
2025-01-02 18:17:49 +0100

    Main Frames Buttons update

--- Frames/MangFrames_Tabs.lua
+++ Frames/MangFrames_Tabs.lua
@@ -47,7 +47,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 35,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -74,7 +74,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 30,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -100,7 +100,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 30,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -126,7 +126,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 30,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -153,7 +153,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 35,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -179,7 +179,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 40,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -205,7 +205,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 30,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -232,7 +232,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 40,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -258,7 +258,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 30,
+      width = 70,
       height = 20
     },
     setpoint = {
@@ -284,7 +284,7 @@ function AzerothAdmin:CreateTabs()
       }
     },
     size = {
-      width = 35,
+      width = 70,
       height = 20
     },
     setpoint = {
